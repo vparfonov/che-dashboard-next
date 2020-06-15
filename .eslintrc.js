16f70203503c920bab5d967a3136d8c903c2496a
Oleksii Kurinnyi <okurinny@redhat.com>
2020-06-15 15:38:20 +0300

    added eslint rules for react hooks
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,9 +17,10 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
   ],
   ignorePatterns: [
     '.github/',
